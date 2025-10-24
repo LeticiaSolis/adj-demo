6d39a455afd402f1ed730282bdb4a5e93170e82f
LeticiaSolis <20223tn112@utez.edu.mx>
2025-10-24 15:23:11 -0600

    Cambios en archivos

--- server/src/main/java/jcgr/demo/server/modules/test/TestController.java
+++ server/src/main/java/jcgr/demo/server/modules/test/TestController.java
@@ -1,26 +1,25 @@
-import java.util.HashMap;
+package jcgr.demo.server.modules.test;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/adj-api/test")
 @CrossOrigin(origins = "*")
-public  class TestController {
+public class TestController {
     @GetMapping("")
-    public  ResponseEntity<?> getMessage(){
+    public ResponseEntity<?> getMessage() {
         HashMap<String, Object> response = new HashMap<>();
         response.put("data", "OK");
         response.put("message", "API funcionando");
-
-        return  new ResponseEntity<>(
+        return new ResponseEntity<>(
                 response,
-                HttpStatus.OK
-        );
+                HttpStatus.OK);
     }
 }
